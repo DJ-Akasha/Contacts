d6af124658900f87e9715a8a1f9a236226597c64
DJ-Akasha <simonelisa@gmail.com>
2021-08-20 13:19:06 +0100

    Added a Floating Action Button

--- app/src/main/java/com/powjunkie/contacts/MainActivity.kt
+++ app/src/main/java/com/powjunkie/contacts/MainActivity.kt
@@ -2,10 +2,20 @@ package com.powjunkie.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
+
 }
