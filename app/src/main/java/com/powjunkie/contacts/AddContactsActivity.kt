4465bb94546b21c0c5009b307748cfb3178523ff
DJ-Akasha <simonelisa@gmail.com>
2021-08-21 17:00:16 +0100

    Added the add photo functionality to the AddContactsActivity.kt

--- app/src/main/java/com/powjunkie/contacts/AddContactsActivity.kt
+++ app/src/main/java/com/powjunkie/contacts/AddContactsActivity.kt
@@ -1,7 +1,14 @@
 package com.powjunkie.contacts
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class AddContactsActivity : AppCompatActivity() {
@@ -10,12 +17,20 @@ class AddContactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_contacts)
         /**
-         * Create a FAB that opens a camera app
+         * TODO - Store the user's input when they press the Submit button and return to MainActivity
          *
-         * val fab: View = findViewById(R.id.fab)
-        fab.setOnClickListener {
-        startActivity(Intent(this@MainActivity, AddContactsActivity::class.java))
-        }
          */
+        val ivPhoto: ImageView = findViewById(R.id.avatarImage)
+        val fab: View = findViewById(R.id.fabAddPhoto)
+
+        // Sets the image the user chose to the avatarImage ImageView
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent())  { uri: Uri? ->
+            ivPhoto.setImageURI(uri)
+        }
+
+        // Floating Action Button that opens the gallery app so user can select a photo
+        fab.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 }
